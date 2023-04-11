6e29791abbc0252c00a0f659393abe2272aa158f
Harshith Vasireddy <harshithvasireddy2004@gmail.com>
2023-04-11 22:57:04 +0530

    meow

--- bloodManagement/urls.py
+++ bloodManagement/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'bloodManagement'
 urlpatterns=[
-    
+    path('donor/', views.donor, name='donor')
 ]
--- bloodManagement/views.py
+++ bloodManagement/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def donor(request):
+    if request.method == 'GET':
+        return render(request, 'bloodManagement/donor.html')
